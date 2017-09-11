448e70603357cfb81db024eef473c72f8951b433
Bernat Borrás <bernatbor15@gmail.com>
2017-09-11 09:55:44 +0200

    Add Data to string

--- lib/weight_list.dart
+++ lib/weight_list.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
-class WeightListScreen extends StatelessWidget {
+class WeightListScreen extends StatefulWidget {
+  @override
+  State<WeightListScreen> createState() => new WidgetListState();
+}
+
+class WidgetListState extends State<WeightListScreen> {
+  final List<WeightData> _weights = <WeightData>[];
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(title: new Text("Weight Control")),
-        body: new Container(),
+        body: new Container(
+            child:
+                _weights.length > 0 ?
+                new Text(_weights[0].value.toString()) :
+                new Text("No Data")),
         floatingActionButton: new FloatingActionButton(
             child: new Icon(
               Icons.add,
@@ -17,6 +28,17 @@ class WeightListScreen extends StatelessWidget {
   }
 
   onValue(double value) {
+    WeightData wd = new WeightData(value: value, date: new DateTime.now());
 
+    setState(() {
+      _weights.add(wd);
+    });
   }
+}
+
+class WeightData {
+  final double value;
+  final DateTime date;
+
+  WeightData({this.value, this.date});
 }
